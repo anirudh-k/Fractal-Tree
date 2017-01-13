691f50474d84f5729e5e34d79f1328f548c5f9f8
anirudhkaushik <kaushik.an@husky.neu.edu>
2017-01-13 12:50:36 -0500

    added functions to draw a fractal tree

--- sketch.js
+++ sketch.js
@@ -1,7 +1,52 @@
+var angle = 0;
+
+var slider;
+
+// required for p5.js project
+// creates canvas and angle slider
 function setup() {
-  
+  createCanvas(800, 800);
+  slider = createSlider(0, PI, PI / 4, 0.01);
 }
 
+// required for p5.js project
+// initiates the branch recursion for drawing the tree
 function draw() {
-  
+  background(51);
+
+  angle = slider.value();
+
+  stroke(255);
+
+  translate(width / 2, height);
+
+  branch(200);
+}
+
+// recursive method that draws a tree with a variable angle
+// @param len the length of the branch of the tree
+function branch(len) {
+	strokeWeight(ceil(len / 10));
+
+	line(0, 0, 0, -len);
+	translate(0, -len);
+	
+	if (len > 2) {
+		// saves the previous translate location (the fork point in the tree --- Y)
+		push();
+
+		rotate(angle);
+		branch(len * (2 / 3));
+
+		// restores to the previous translate location
+		pop();
+		// saves the previous translate location
+		push();
+
+		rotate(-angle);
+		branch(len * (2 / 3));
+
+		pop();
+	}
+	//line(0, 0, 0, -len * (2/3));
 }
